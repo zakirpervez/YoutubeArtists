904b990523e5398d26c465b9951da5d904f5696f
Zakir Pervez <zakir.mohammad@valuelabs.com>
2021-06-15 17:37:16 +0530

    youtube player integration

--- app/src/main/java/com/valuelabs/youtubeartists/ui/activities/MainActivity.kt
+++ app/src/main/java/com/valuelabs/youtubeartists/ui/activities/MainActivity.kt
@@ -4,26 +4,52 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
 import com.valuelabs.youtubeartists.R
 import com.valuelabs.youtubeartists.databinding.ActivityMainBinding
 import com.valuelabs.youtubeartists.models.TempModel
+import com.valuelabs.youtubeartists.ui.view.showLongToast
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
-@AndroidEntryPoint
-class MainActivity : RootActivity() {
-    @Inject
-    lateinit var tempModel: TempModel
+class MainActivity : YouTubeBaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val mainBinding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        mainBinding.greetTextView.text = tempModel.greet
+        setupYoutubePlayer(mainBinding)
     }
 
-    companion object{
-        fun buildIntent(from: Context): Intent{
+    private fun setupYoutubePlayer(mainBinding: ActivityMainBinding) {
+        val youtubeInitListener = object : YouTubePlayer.OnInitializedListener {
+            override fun onInitializationSuccess(
+                provider: YouTubePlayer.Provider?,
+                player: YouTubePlayer?,
+                p2: Boolean
+            ) {
+                player?.loadVideo("VQttXb6qE6k")
+            }
+
+            override fun onInitializationFailure(
+                provider: YouTubePlayer.Provider?,
+                result: YouTubeInitializationResult?
+            ) {
+                showLongToast("Something went wrong")
+            }
+        }
+        mainBinding.playButton.setOnClickListener {
+            mainBinding.youtubePlayerView.initialize(
+                "AIzaSyBK3_mBRuQnrQo4QeGcQ7jFQiwmV83c7ck",
+                youtubeInitListener
+            )
+        }
+    }
+
+    companion object {
+        fun buildIntent(from: Context): Intent {
             return Intent(from, MainActivity::class.java)
         }
     }
